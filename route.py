3cb3cf873ddad576e709732c49cacd6605103b81
nave pirata <anonyme201504@gmail.com>
2024-07-28 20:17:30 -0300

    recipe

--- route.py
+++ route.py
@@ -18,7 +18,7 @@ import sys
 class Route():
     def __init__(self):
         self.dbUsers=User()
-        self.Program=Directory("mme AI")
+        self.Program=Directory("recettes ruby /python")
         self.Program.set_path("./")
         self.mysession={"notice":None,"email":None,"name":None}
         self.dbCountry=Country()
